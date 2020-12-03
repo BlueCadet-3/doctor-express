6a89bcee7eb89ed5ff3caa8f132c0326159c5e81
BlueCadet-3 <martysilha@gmail.com>
2020-12-03 00:43:28 -0600

    gh-pages

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Doctor Express' });
 });
 
 // Google OAuth login route
