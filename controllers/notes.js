dd8704d9d514d6473fdfb55de7fe0dfa376394ae
BlueCadet-3 <martysilha@gmail.com>
2020-12-07 14:30:43 -0600

    add note functionality

--- controllers/notes.js
+++ controllers/notes.js
@@ -7,12 +7,24 @@ module.exports = {
 
 function create(req, res) {
   Patient.findById(req.params.id, function(err, patient) {
+    req.body.user = req.user._id;
+    req.body.userName = req.user.name;
+    req.body.userAvatar = req.user.avatar;
     patient.notes.push(req.body);
     patient.save(function(err) {
-      res.redirect(`/patients/${user._id}`);
+      res.redirect(`/patients/${patient._id}`);
     });
   });
 }
+
+// function create(req, res) {
+//   Patient.findById(req.params.id, function(err, patient) {
+//     patient.notes.push(req.body);
+//     patient.save(function(err) {
+//       res.redirect(`/patients/${patient._id}`);
+//     });
+//   });
+// }
 
 // function create(req, res) {
 //   userId = req.params.id;
