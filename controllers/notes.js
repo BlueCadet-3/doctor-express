4cf95aacde4e65cef5f9332ac2e4cd70396ee6d0
BlueCadet-3 <martysilha@gmail.com>
2020-12-07 16:20:13 -0600

    delete note functionality

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,13 +1,13 @@
 const Patient = require('../models/patient');
 
 module.exports = {
-  // new: newNote,
-  create
+  create,
+  delete: deleteNote
 };
 
 function create(req, res) {
   Patient.findById(req.params.id, function(err, patient) {
-    req.body.user = req.user._id;
+    req.body.user = req.user.id;
     req.body.userName = req.user.name;
     req.body.userAvatar = req.user.avatar;
     patient.notes.push(req.body);
@@ -17,20 +17,16 @@ function create(req, res) {
   });
 }
 
-// function create(req, res) {
-//   Patient.findById(req.params.id, function(err, patient) {
-//     patient.notes.push(req.body);
-//     patient.save(function(err) {
-//       res.redirect(`/patients/${patient._id}`);
-//     });
-//   });
-// }
-
-// function create(req, res) {
-//   userId = req.params.id;
-//   req.body.user = userId;
-//   Note.create(req.body, function(err, note) {
-//     console.log(userId + "" + req.body);
-//     res.redirect(`/patients/${userId}`);
-//   });
-// }
+function deleteNote(req, res, next) {
+  Patient.findOne({'notes._id': req.params.id})
+  .then(function(patient) {
+    const note = patient.notes.id(req.params.id);
+    if(!note.user.equals(req.user._id)) return res.redirect(`/patients/${patient._id}`);
+    note.remove();
+    patient.save().then(function() {
+      res.redirect(`/patients/${patient._id}`);
+    }).catch(function(err) {
+      return next(err);
+    });
+  });
+}
--- routes/notes.js
+++ routes/notes.js
@@ -5,5 +5,6 @@ const notesCtrl = require('../controllers/notes');
 
 // router.get('/patients/:id/notes/new', notesCtrl.new);
 router.post('/patients/:id/notes', notesCtrl.create);
+router.delete('/notes/:id', notesCtrl.delete);
 
 module.exports = router;
