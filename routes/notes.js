997703f6873141cf7f208daaf337df87b4732850
BlueCadet-3 <martysilha@gmail.com>
2020-12-08 12:17:09 -0600

    update note functionality

--- routes/notes.js
+++ routes/notes.js
@@ -5,6 +5,6 @@ const notesCtrl = require('../controllers/notes');
 
 router.post('/patients/:id/notes', notesCtrl.create);
 router.delete('/notes/:id', notesCtrl.delete);
-router.patch('/notes/:id', notesCtrl.update);
+router.put('/notes/:id', notesCtrl.update);
 
 module.exports = router;
