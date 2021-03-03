0ba1fc9aa8c91b3b08504686e8c30a7b59bfba30
BlueCadet-3 <martysilha@gmail.com>
2021-03-03 15:16:42 -0600

    add favicon

--- server.js
+++ server.js
@@ -1,74 +1,78 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var session = require('express-session');
-var passport = require('passport');
-var methodOverride = require('method-override');
+var session = require("express-session");
+var passport = require("passport");
+var methodOverride = require("method-override");
+var favicon = require("serve-favicon");
 
 // Always process the .env file early
-require('dotenv').config();
+require("dotenv").config();
 // Run the database configuration code
-require('./config/database');
+require("./config/database");
 // Run the passport configuration code
-require('./config/passport');
+require("./config/passport");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var patientsRouter = require('./routes/patients');
-var notesRouter = require('./routes/notes');
-var medicationsRouter = require('./routes/medications');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var patientsRouter = require("./routes/patients");
+var notesRouter = require("./routes/notes");
+var medicationsRouter = require("./routes/medications");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
-app.use(session({
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 // relieves from having to pass user: req.user every time a view is rendered
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.locals.user = req.user;
   next();
 });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/patients', patientsRouter);
-app.use('/', notesRouter);
-app.use('/', medicationsRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/patients", patientsRouter);
+app.use("/", notesRouter);
+app.use("/", medicationsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
