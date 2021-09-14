02ab5547c9ce368641bd4d6ad4fe3a2bc59929eb
Martin Silha <martysilha@gmail.com>
2021-09-14 00:52:28 -0500

    database config to ts

--- build/config/database.js
+++ build/config/database.js
@@ -1,11 +1,11 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 });
 // shortcut to mongoose.connection object
 var db = mongoose.connection;
-db.on('connected', function () {
+db.on("connected", function () {
     console.log("Connected to " + db.name + " at " + db.host + ":" + db.port);
 });
