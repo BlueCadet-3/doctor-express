5118590e676b93da219fcc54b98803518c31345e
Martin Silha <martysilha@gmail.com>
2021-10-20 21:59:32 -0500

    fix user id in note creation

--- controllers/notes.js
+++ controllers/notes.js
@@ -8,62 +8,33 @@ module.exports = {
 };
 
 async function create (req, res) {
-	// console.log(":::createNoteStart", note);
-	const note = await Note
-		.create({
-			note: req.body.note,
-			user: req.user._id
-		});
+	const doc = {
+		note: req.body.note,
+		user: req.params.id
+	};
+	const note = await Note.create(doc);
 	const user = await User.findById(req.params.id);
 	user.patient.notes.push(note);
-	// console.log(":::createNoteEnd", note);
 	await user.save((err) => {
 		if (err) return res.render('error/error', { error: err });
-		res.redirect(`/users/${req.user.role}/${req.user._id}`);
+		res.redirect(`/users/${req.user.role}/${req.params.id}`);
 	});
 }
 
 async function update (req, res) {
+	const filter = { _id: req.params.id };
 	const update = { note: req.body.text };
-	console.log(":::updateNote", req.body);
-	console.log(":::updateNote", req.user);
-	const note = await Note.findOneAndUpdate({ _id: req.params.id }, update, { new: true });
+	const options = { new: true };
+	const note = await Note.findOneAndUpdate(filter, update, options);
 	await note.save((err) => {
 		if (err) return res.render('error/error', { error: err });
 		res.redirect(`/users/${req.user.role}/${req.user._id}`);
 	});
-	console.log(":::updateNote", note);
 }
 
-// function update (req, res) {
-// 	console.log(":::updateNote");
-// 	// Using dot syntax to query on the property of a subdoc
-// 	User.findOne({ 'patient.notes._id': req.params.id }, function (err, user) {
-// 		// Find the comment subdoc using the id method on Mongoose arrays
-// 		const noteSubdoc = patient.notes.id(req.params.id);
-// 		// Ensure that the comment was created by the logged in user
-// 		// if(!noteSubdoc.userId.equals(req.user._id)) return res.redirect(`/patients/${patient._id}`);
-// 		// Update the text of the comment
-// 		noteSubdoc.note = req.body.text;
-// 		// Save the updated patient
-// 		user.save(function (err) {
-// 			if (err) return res.render('error/error', { error: err });
-// 			// Redirect back to the patient's show view
-// 			res.redirect(`/users/${req.user.role}/${user._id}`);
-// 		});
-// 	});
-// }
-
-function deleteNote (req, res, next) {
-	Patient.findOne({ 'notes._id': req.params.id })
-		.then(function (patient) {
-			const note = patient.notes.id(req.params.id);
-			if (!note.user.equals(req.user.id)) return res.redirect(`/patients/${patient._id}`);
-			note.remove();
-			patient.save().then(function () {
-				res.redirect(`/patients/${patient._id}`);
-			}).catch(function (err) {
-				return next(err);
-			});
-		});
+async function deleteNote (req, res) {
+	const filter = { _id: req.params.id };
+	const options = {};
+	await Note.findOneAndDelete(filter, options);
+	res.redirect(`/users/${req.user.role}/${req.user._id}`);
 }
