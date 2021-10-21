aea9922dcc9d0fb3f8043616a3e76d2926533833
Martin Silha <martysilha@gmail.com>
2021-10-21 01:42:02 -0500

    use async

--- public/js/onboard.js
+++ public/js/onboard.js
@@ -1,15 +1,14 @@
 const roleSelect = document.getElementById("role-select");
-
 const patientForm = document.getElementById("new-patient");
 const doctorForm = document.getElementById("new-doctor");
 
 changeForm("none");
 
-roleSelect.addEventListener('change', function () {
+roleSelect.addEventListener('change', async function () {
 	changeForm(this.value);
 });
 
-function changeForm (role) {
+async function changeForm (role) {
 	if (role === "none") {
 		doctorForm.style.display = "none";
 		patientForm.style.display = "none";
