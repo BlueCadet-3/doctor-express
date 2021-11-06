de7fa8945f78dcdf1928863817e66746eb514a45
Martin Silha <martysilha@gmail.com>
2021-11-06 14:51:32 -0500

    improve edit note visuals

--- public/js/edit-comment.js
+++ public/js/edit-comment.js
@@ -1,8 +1,13 @@
-function editNote(editNoteId) {
-  let editNote = document.getElementById(editNoteId);
+function editNote (editNoteId) {
+  const editNote = document.getElementById(editNoteId);
+  const spanNote = document.getElementById(`span-${editNoteId}`);
+  console.log(":::editNote", editNote);
+  console.log(":::editNote", spanNote.style);
   if (editNote.style.display === "none") {
     editNote.style.display = "block";
+    spanNote.style.display = "none";
   } else {
     editNote.style.display = "none";
+    spanNote.style.display = "block";
   }
 }
